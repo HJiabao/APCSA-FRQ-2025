18d83958e5f11f2911f82f535deb4525e624fba4
HJiabao <Jiabaoh2@nycstudents.net>
2025-05-13 09:43:14 -0400

    Update partB.java

--- Q1/partB.java
+++ Q1/partB.java
@@ -1,1 +1,10 @@
-
+public int dogWalkShift(int startHour, int endHour){
+        int amount=0;
+        for(int i=startHour;i<=endHour;i++){
+            if(walkDogs(i) == maxDogs || (i>=9 && i<=17)){
+                amount+=3;
+            }
+            amount+= walkDogs(i)*5;
+        }
+        return amount;
+    }
