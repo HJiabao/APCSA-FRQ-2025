c6fc3e174ddbc71dc2b0a9f676a1b553acee04a4
HJiabao <Jiabaoh2@nycstudents.net>
2025-05-13 09:42:35 -0400

    Delete part1A.java

--- part1A.java
+++
@@ -1,8 +0,0 @@
-public int walkDogs(int hour){
-        int amount=company.numAvailableDogs(hour);
-        if(amount>=maxDogs){
-            amount=maxDogs;
-        }
-        company.updateDogs(hour,amount);
-        return amount;
-    }
