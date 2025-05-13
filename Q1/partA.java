1b9a8d78f1bcb3b92d4b58ad695009212bbf3aa4
HJiabao <Jiabaoh2@nycstudents.net>
2025-05-13 09:42:19 -0400

    Update partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -1,1 +1,8 @@
-
+public int walkDogs(int hour){
+        int amount=company.numAvailableDogs(hour);
+        if(amount>=maxDogs){
+            amount=maxDogs;
+        }
+        company.updateDogs(hour,amount);
+        return amount;
+    }
