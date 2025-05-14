6e779ba412f37762b0bd8debfa3f21700a5752ec
HJiabao <Jiabaoh2@nycstudents.net>
2025-05-14 09:54:35 -0400

    Update partA.java

--- Q4/partA.java
+++ Q4/partA.java
@@ -1,1 +1,8 @@
-
+public SumOrSameGame(int numRows, int numCols){
+  puzzle = new int[numRows][numCols];
+  for(int i=0;i<puzzle.length;i++){
+    for(int j=0;j<puzzle[i].length;j++{
+      puzzle[i][j]=(int) (Math.random() * 9) + 1;
+    }
+  }
+}
