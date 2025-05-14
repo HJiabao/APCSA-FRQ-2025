b6d9d2ecfa3acb7bc812df8ff6471b7a542d1f9b
HJiabao <Jiabaoh2@nycstudents.net>
2025-05-14 09:32:20 -0400

    Update partA.java

--- Q3/partA.java
+++ Q3/partA.java
@@ -1,3 +1,7 @@
 public Round(String[] name){
-  
+  competitorlist = new Arraylist<Competitor>();
+  for(int i=0;i<name.length;i++){
+    Competitor temp = new Competitor(name[i],i+1);
+    competitorlist.add(temp);
+  }
 }
