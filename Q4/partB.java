7b28a56837544c7b8a9c7b416e3e49889d119a34
HJiabao <Jiabaoh2@nycstudents.net>
2025-05-14 10:04:36 -0400

    Update partB.java

--- Q4/partB.java
+++ Q4/partB.java
@@ -1,7 +1,12 @@
 public boolean clearPair(int row, int col){
-  for(int i=0;puzzle.length;i++){
-    for(int j=0;puzzle[j].length;j++){
-      
+  for(int i=row;puzzle.length;i++){
+    for(int j=0;puzzle[i].length;j++){
+      if(puzzle[i][j]==puzzle[row][col] || puzzle[i][j+puzzle[row][col]==10){
+        puzzle[i][j]=0;
+        puzzle[row][col]=0;
+        return true;
+      }
     }
   }
+  return false;
 }
