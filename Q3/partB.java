e382fe47827a917bf05e72b445c7f1df4e5a0ba8
HJiabao <Jiabaoh2@nycstudents.net>
2025-05-14 09:48:41 -0400

    Update partB.java

--- Q3/partB.java
+++ Q3/partB.java
@@ -1,1 +1,15 @@
-
+public Arraylist<Match> buildMatches(){
+  int i=0;
+  int j=competitorList.size()-1;
+  ArrayList<Match> matches = new ArrayList<Match>();
+  if(j+1 % 2 == 1){
+    i++;
+  }
+  while(i != (j+1)/2){
+    Match temp= new Match(competitorList.get(i),competitorList.get(j));
+    matches.add(temp);
+    i++;
+    j--:
+  }
+  return matches;
+}
